70ebb7212eb33005afdb490f43bb7806a0f4808f
Melissa Real <melissa.real@hotmail.com>
2019-04-01 15:41:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+class Driver{
+  constructor(name, startDate){
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
 
+  yearsExperienceFromBeginningOf(endDate){
+    return endDate - this.startDate.getFullYear();
+  }
+}
+
+class Route{
+  constructor(beginningLocation,endingLocation){
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled(){
+   let eastWest = [
+    '1st Avenue',
+    '2nd Avenue',
+    '3rd Avenue',
+    'Lexington Avenue',
+    'Park',
+    'Madison Avenue',
+    '5th Avenue'
+  ];
+  }
+
+  estimatedTime(){
+
+  }
+}
