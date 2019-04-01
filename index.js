956b534d72b331fb30e199e3b7577f03a24a4ee0
Melissa Real <melissa.real@hotmail.com>
2019-04-01 15:59:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ class Route{
 //indexOf points to the array location. So we know the 2nd avenue will be on the 2nd index but because arrays start at 0 we add+1
 //then we grab the vertical location
 
-  let horizontal =  (eastWest.indexOf(this.beginningLocation.horizontal) + 1) - (eastWest.indexOf(this.endingLocation.horizontal) + 1) ;
-  let vertical = this.beginningLocation.vertical - this.endingLocation.vertical ;
+  let horizontal = (eastWest.indexOf(this.endingLocation.horizontal) + 1) - (eastWest.indexOf(this.beginningLocation.horizontal) + 1);
+  let vertical = this.endingLocation.vertical - this.beginningLocation.vertical;
 
     return horizontal + vertical;
 
